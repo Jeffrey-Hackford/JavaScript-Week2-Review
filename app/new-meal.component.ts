e572825b9aad4f0c1eefb4b66d822820ae558e9f
Jeffrey Hackford <jeffrey.hackford@gmail.com>
2016-06-03 10:22:29 -0700

    update user form and addMeal method in new-meal component

--- app/new-meal.component.ts
+++ app/new-meal.component.ts
@@ -2,12 +2,15 @@ import { Component, EventEmitter } from 'angular2/core';
 import { Meal } from './meal.model';
 
 @Component({
-  selector: 'new-task',
+  selector: 'new-meal',
   outputs: ['onSubmitNewMeal'],
   template: `
   <div class="container form">
-    <input placeholder="Add Name of Food" class="col-xs-5 col-xs-offset-1">
-    <button (click)="addMeal()">Enter</button>
+    <input placeholder="Add Name of Food" class="col-xs-5 col-xs-offset-2" #MealName>
+    <input placeholder="Add Details of Food" class="col-xs-5 col-xs-offset-2" #MealDetails>
+    <input placeholder="Add Calories of Food" class="col-xs-5 col-xs-offset-2" #MealCalories>
+    <button (click)="addMeal(MealName, MealDetails, MealCalories)">Enter</button>
+  </div>
   `
 })
 export class NewMealComponent{
@@ -15,7 +18,7 @@ export class NewMealComponent{
   constructor() {
     this.onSubmitNewMeal = new EventEmitter();
   }
-  addMeal(){
-    going to do something... soon;
+  addMeal(Name: HTMLInputElement, Details: HTMLInputElement, Calories: HTMLInputElement){
+    console.log(Name.value, Details.value, Calories.value);
   }
 }
